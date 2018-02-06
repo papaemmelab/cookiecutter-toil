d8bb439d7423e1d452bad8ec08f761b99726b3d4
Juan Esteban Arango <arangooj@mskcc.org>
2018-02-05 20:15:12 -0500

    :wrench: add hooks to remove unused files in click projects.

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,19 @@
+"""
+Hook to remove unused files.
+
+See https://github.com/audreyr/cookiecutter/issues/723
+"""
+
+import os
+import shutil
+
+
+def remove(filepath):
+    if os.path.isfile(filepath):
+        os.remove(filepath)
+    elif os.path.isdir(filepath):
+        shutil.rmtree(filepath)
+
+if '{{cookiecutter.cli_type}}' == 'click':
+    remove(os.path.join('{{cookiecutter.project_slug}}', 'commands.py'))
+    remove(os.path.join('{{cookiecutter.project_slug}}', 'jobs.py'))
