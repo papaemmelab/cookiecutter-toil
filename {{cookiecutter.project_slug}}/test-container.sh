70cff736bddbfda50439bdbdff3bce94b411244d
Juan Medina <juansantiago.medina@gmail.com>
2018-03-18 01:36:01 -0400

    🐛 fix tests

--- {{cookiecutter.project_slug}}/test-container.sh
+++ {{cookiecutter.project_slug}}/test-container.sh
@@ -5,8 +5,8 @@
 if [ "$1" = "--skip-build" ]; then
     echo "skipping build..."
 else
-    echo "building image, to skip run with --skip-build"
-    docker build -t test-image .
+    echo "building image, to skip run with --skip-build..."
+    docker build -q -t test-image .
 fi
 
 # see https://explainshell.com/explain?cmd=set+-euxo%20pipefail
@@ -17,15 +17,15 @@ find . -name '*.pyc' -exec rm {} +
 find . -name '__pycache__' -exec rm -rf {} +
 
 # test image
+echo "testing docker image..."
 docker run --rm test-image --version
 docker run --rm --entrypoint "" -v `pwd`:/test -w /test \
-    test-image bash -c "pip install tox && tox"
+    test-image bash -c "pip install tox > /dev/null 2>&1 && tox"
 
 # move container coverage paths to local, see .coveragerc [paths] and this comment:
 # https://github.com/pytest-dev/pytest-cov/issues/146#issuecomment-272971136
-command -v coverage > /dev/null 2>&1 || { pip install coverage }
-
 echo "combining container coverage..."
+command -v coverage > /dev/null 2>&1 || pip install coverage
 mv .coverage .coverage.tmp
 coverage combine --append
 
