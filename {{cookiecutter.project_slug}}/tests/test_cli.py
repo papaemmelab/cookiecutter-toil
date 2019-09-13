5066317df5350870f487f0a9343d5cba0f573e7b
Juan Esteban Arango Ossa <juanes.ao@gmail.com>
2019-09-13 14:06:49 -0400

    🐛fix click cli test

--- {{cookiecutter.project_slug}}/tests/test_cli.py
+++ {{cookiecutter.project_slug}}/tests/test_cli.py
@@ -21,7 +21,7 @@ def test_main():
     """Sample test for main command."""
     message = "This is a test message for the Universe."
     runner = CliRunner()
-    params = ["message", message]
+    params = ["--message", message]
     result = runner.invoke(cli.main, params)
     assert message in result.output
 {% endif %}
